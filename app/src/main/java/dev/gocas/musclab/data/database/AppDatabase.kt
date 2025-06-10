651b828114cf5706d34b1339561c5d50bc2aa0f2
GocasPT <gocasousa2013@gmail.com>
2025-06-10 16:15:56 +0100

    Hilt + MVVM Structure #13

--- app/src/main/java/dev/gocas/musclab/data/database/AppDatabase.kt
+++ app/src/main/java/dev/gocas/musclab/data/database/AppDatabase.kt
@@ -5,7 +5,13 @@ import androidx.room.RoomDatabase
 import dev.gocas.musclab.data.database.dao.ExerciseDao
 import dev.gocas.musclab.data.database.model.ExerciseEntity
 
-@Database(entities = [ExerciseEntity::class], version = 1)
+@Database(
+    entities = [
+        ExerciseEntity::class
+               ],
+    version = 1,
+    exportSchema = true
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun exerciseDao(): ExerciseDao
 }
