6ade2b910b23a3c8e8901b5e5cba4b792b1719fa
GocasPT <gocasousa2013@gmail.com>
2025-06-10 15:19:35 +0100

    Config Room Database + Repository Pattern example #12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -51,6 +52,10 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.kotlinx.coroutines.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/dev/gocas/musclab/data/database/dao/ExerciseDao.kt
@@ -0,0 +1,25 @@
+package dev.gocas.musclab.data.database.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+import dev.gocas.musclab.data.database.model.ExerciseEntity;
+import kotlinx.coroutines.flow.Flow;
+
+@Dao
+interface ExerciseDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(exercise: ExerciseEntity);
+
+    @Update
+    suspend fun update(exercise: ExerciseEntity)
+
+    @Delete
+    suspend fun delete(exercise: ExerciseEntity)
+
+    @Query("SELECT * FROM exercise ORDER BY id DESC")
+    fun getAllExercises(): Flow<List<ExerciseEntity>>
+}
